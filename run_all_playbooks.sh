838422a6dfa1a05575051aab851ae9c42fbb8d1a
Vince Gonzalez <vince.gonzalez@amd.com>
2013-11-20 23:13:37 +0000

    trying to generalize the variables so that the playbooks are more easily portable to new environments.

--- run_all_playbooks.sh
+++ run_all_playbooks.sh
@@ -11,15 +11,15 @@ fi
 
 # run an ansible "ping" to test whether we can gain access to the 
 # servers.
-ansible selab -m ping -u root -i hosts 2>&1 > /dev/null
+ansible openstack-cluster -m ping -u root -i hosts 2>&1 > /dev/null
 ANSIBLE_PING_RC="$?"
 
 # If we can't get access, try installing our key in authorized_keys 
 # on the target servers.
 if [ $ANSIBLE_PING_RC != 0 ]; then
 	echo "Installing public key."
-	ansible selab -m authorized_key -u root -i hosts -k -c paramiko -a "user=root key='$KEY'"
+	ansible openstack-cluster -m authorized_key -u root -i hosts -k -c paramiko -a "user=root key='$KEY'"
 fi
 
 # having added the public key, perform the install.
-ansible-playbook -i hosts -l selab site.yml
+ansible-playbook -i hosts site.yml
