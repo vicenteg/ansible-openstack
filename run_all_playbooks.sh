0947ac7253a2e7fc82436b7d3cdb804d7843f416
vicentg <vince.gonzalez@gmail.com>
2013-12-09 09:40:01 -0500

    keystone now HA capable

--- run_all_playbooks.sh
+++ run_all_playbooks.sh
@@ -11,14 +11,14 @@ fi
 
 # run an ansible "ping" to test whether we can gain access to the 
 # servers.
-ansible openstack-cluster -m ping -u root -i hosts 2>&1 > /dev/null
+ansible openstack_cluster -m ping -u root -i hosts 2>&1 > /dev/null
 ANSIBLE_PING_RC="$?"
 
 # If we can't get access, try installing our key in authorized_keys 
 # on the target servers.
 if [ $ANSIBLE_PING_RC != 0 ]; then
 	echo "Installing public key."
-	ansible openstack-cluster -m authorized_key -u root -i hosts -k -c paramiko -a "user=root key='$KEY'"
+	ansible openstack_cluster -m authorized_key -u root -i hosts -k -c paramiko -a "user=root key='$KEY'"
 fi
 
 # having added the public key, perform the install.
